862b9bad1f1a4aebe0c135ae7df6f5b0ace83b13
Alan Christianson <git@c10n.net>
2015-02-19 14:20:38 -0700

    Begin building base planner class.

--- cutplanner/planner.py
+++ cutplanner/planner.py
@@ -1,12 +1,20 @@
+import collections
+
+# simple structure to keep track of a specific piece
+Piece = collections.namedtuple('Piece', 'id, length')
+
 class Planner(object):
 
-    def __init__(self, sizes, needed):
+    def __init__(self, sizes, needed, loss=0.25):
         self.stock = []
         self.stock_sizes = sorted(sizes)
         self.pieces_needed = needed
+        self.cut_loss = loss
+        self.cur_stock = None
 
-    def getLargestStock(self):
+    def get_largest_stock(self):
         return self.stock_sizes[-1]
 
-
-
+    def cut_piece(self, piece):
+        """ Record the cut for the given piece """
+        cur_stock.cut(piece, self.cut_loss)
