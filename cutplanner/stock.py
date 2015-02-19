93e701e134a8584d4b9dd8988690b3a3e067dc8a
Alan Christianson <git@c10n.net>
2015-02-19 14:15:08 -0700

    Start fleshing out Stock class

--- cutplanner/stock.py
+++ cutplanner/stock.py
@@ -6,4 +6,24 @@ class Stock(object):
         self.length = length
         self.remaining_length = length
 
+    @property
+    def used_length(self):
+        return self.length - self.remaining_length
 
+    def cut(self, piece, loss=0.0):
+        """ Cut given amount from this stock """
+        if (loss + piece.length) > self.remaining_length:
+            raise ValueError("Stock too small to cut piece")
+
+        self.pieces.append(piece)
+        self.remaining_length -= (loss + piece.length)
+        return self.remaining_length
+
+    def shrink(self, new_length):
+        """ Attempt to reduce stock size, to minimize waste when possible. """
+        if new_length < self.used_length:
+            return None
+
+        self.remaining_length = new_length - self.used_length
+        self.length = new_length
+        return new_length
