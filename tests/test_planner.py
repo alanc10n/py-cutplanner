1f8448a37b6121913187b55bc979d426157deed2
Alan Christianson <git@c10n.net>
2015-02-19 14:21:02 -0700

    Start building planner tests

--- tests/test_planner.py
+++ tests/test_planner.py
@@ -4,7 +4,14 @@ import unittest
 class TestPlanner(unittest.TestCase):
 
     def setUp(self):
+        sizes = [80, 120]
+        needed = [10, 25, 75]
+        loss = 0.25
+        self.planner = cutplanner.Planner(sizes, needed, loss)
 
+    def test_get_largest(self):
+        largest = self.planner.get_largest_stock()
+        self.assertEqual(largest, 120)
 
 if __name__ == '__main__':
     unittest.main()
