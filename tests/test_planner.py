ff4b34cda7c0b5bc516d9f9e3689818000301336
Alan Christianson <git@c10n.net>
2015-03-22 16:07:28 -0600

    Update test for largest stock

--- tests/test_planner.py
+++ tests/test_planner.py
@@ -9,8 +9,8 @@ class TestPlanner(unittest.TestCase):
         loss = 0.25
         self.planner = cutplanner.Planner(sizes, needed, loss)
 
-    def test_get_largest(self):
-        largest = self.planner.get_largest_stock()
+    def test_largest_stock(self):
+        largest = self.planner.largest_stock
         self.assertEqual(largest, 120)
 
 if __name__ == '__main__':
