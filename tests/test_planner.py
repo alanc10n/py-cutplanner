a7af6475a3aa8f9c241d4a8fdf3c85e63950f28a
Alan Christianson <git@c10n.net>
2015-03-22 16:37:46 -0600

    Add test for finalize

--- tests/test_planner.py
+++ tests/test_planner.py
@@ -4,7 +4,7 @@ import unittest
 class TestPlanner(unittest.TestCase):
 
     def setUp(self):
-        sizes = [80, 120]
+        sizes = [50, 80, 120]
         needed = [10, 25, 75]
         loss = 0.25
         self.planner = cutplanner.Planner(sizes, needed, loss)
@@ -12,6 +12,13 @@ class TestPlanner(unittest.TestCase):
     def test_largest_stock(self):
         largest = self.planner.largest_stock
         self.assertEqual(largest, 120)
+
+    def test_finalize(self):
+        self.planner.cur_stock = cutplanner.Stock(self.planner.largest_stock)
+        self.planner.cut_piece(cutplanner.Piece(1, 60))
+        self.planner.finalize_stock()
+        self.assertEqual(len(self.planner.stock), 1)
+        self.assertEqual(self.planner.stock[0].length, 80)
 
 if __name__ == '__main__':
     unittest.main()
